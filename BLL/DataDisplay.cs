376a94293267a38d42cea92708c4417caaf2e6ea
聪 刘 <2593466351@qq.com>
2020-05-25 09:44:53 +0800

    增加分页功能

--- BLL/DataDisplay.cs
+++ BLL/DataDisplay.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLL
+﻿namespace BLL
 {
     public class DataDisplay
     {
-        public string conversion(int Number)
+        public string Conversion(int Number)
         {
-            if (Number.ToString().Length >= 5)
-            {
-                
-                return $"{(Number * 10 / 10000) / 10.0f}万";
-            }
-            return Number.ToString();
+            return Number.ToString().Length >= 5 ? $"{Number * 10 / 10000 / 10.0f}万" : Number.ToString();
         }
     }
 }
--- shareMvc/Controllers/HomeController.cs
+++ shareMvc/Controllers/HomeController.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using BLL;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Model;
-using BLL;
 
 namespace shareMvc.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int limit = 6, int page = 1)
         {
             var list = new List<article>
             {
@@ -25,7 +24,14 @@ namespace shareMvc.Controllers
                 new article{ArticleId=1,UserId=1,ArticleName="标题8",Comment=1,Content="缘起最近经常有伙伴问我，作为程序员，有没有好的画图工具推荐，领导说Microsoft Visio画图工具很强，让我们使",Favorite=1,like=1,ReleaseDateTime=DateTime.Now,view=1},
                 new article{ArticleId=1,UserId=1,ArticleName="标题9",Comment=1,Content="缘起最近经常有伙伴问我，作为程序员，有没有好的画图工具推荐，领导说Microsoft Visio画图工具很强，让我们使",Favorite=1,like=1,ReleaseDateTime=DateTime.Now,view=1}
             };
-            return View(list);
+            var total = list.Count % limit != 0 ? (list.Count / limit) + 1 : list.Count / limit;
+            ViewBag.page = page;
+            ViewBag.total = total;
+            ViewBag.ListCount = list.Count;
+            var Paging = list.Skip((page - 1) * limit)
+                             .Take(limit)
+                             .ToList();
+            return View(Paging);
         }
 
         public ActionResult SelectArticle()
